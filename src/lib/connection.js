b2fa753137c7a48dd5f9466ef1a3cf1887a350c6
Ajay Mishra MacBook <mishraajayper12345@gmail.com>
2024-03-14 01:02:55 +0530

    fix

--- src/lib/connection.js
+++ src/lib/connection.js
@@ -1,2 +1,2 @@
-const {username, password} = process.env
-export const connection = "mongodb+srv://"+username+":"+password+"@cluster0.yi9ne8e.mongodb.net/next-database?retryWrites=true&w=majority"
+// const {username, password} = process.env
+export const connection = "mongodb+srv://mishraajayper12345:iAsqLe1SqNLzJtDH@cluster0.yi9ne8e.mongodb.net/next-database?retryWrites=true&w=majority"
